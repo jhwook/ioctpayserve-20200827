3aae0e2c838aa22dc7b87e16b5d6edddf01cac5a
cheny56 <cheny56@naver.com>
2021-01-12 21:42:57 +0900

    fix import-20210112

--- utils-sockets.js
+++ utils-sockets.js
@@ -1,7 +1,8 @@
 
 const cliredisa=require('async-redis').createClient()
+const {KEYNAME_USERID2SOCKETID , KEYNAME_SOCKETID2USERID}=require('./configs/configs-sockets')
 const clearsockets=()=>{ //	cliredisa.del(config.KEYNAME_IP_2_SOCKETID);	cliredisa.del(config.KEYNAME_SOCKETID_2_IP)
-	cliredisa.del(config.KEYNAME_SOCKETID2USERID);	cliredisa.del(config.KEYNAME_USERID2SOCKETID)
+	cliredisa.del(KEYNAME_SOCKETID2USERID);	cliredisa.del(KEYNAME_USERID2SOCKETID)
 }
 const getipsocket=(socket)=>{return socket.request.connection.remoteAddress || socket.handshake.address.address || socket.handshake.headers ['X-FORWARDED-FOR']}
 const binduseridsocketid=(userid,socketid)=>{
